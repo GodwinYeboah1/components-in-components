738ef935a71e3af72358c639524f7196e03be2ee
Godwin Yeboah <gyeboah@naccho.org>
2019-08-29 20:09:51 -0400

    adding component in component app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { NotesComponent } from './notes/notes.component';
+import { TimeComponent } from './time/time.component';
+import { ArtsComponent } from './arts/arts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TasksComponent,
+    NotesComponent,
+    TimeComponent,
+    ArtsComponent
   ],
   imports: [
     BrowserModule
